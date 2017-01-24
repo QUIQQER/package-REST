26ea280c8d9b782f25071a1462f9c209a8a73b3c
Henning Leutz <leutz@pcsg.de>
2017-01-24 10:45:01 +0100

    feat: ProviderInterface -> API to extend the REST

--- src/QUI/REST/Server.php
+++ src/QUI/REST/Server.php
@@ -34,6 +34,8 @@ class Server
 
     /**
      * Server constructor.
+     *
+     * @param array $config - optional
      */
     public function __construct($config = array())
     {
@@ -49,6 +51,36 @@ class Server
             );
 
             return $Logger;
+        };
+
+        $container['errorHandler'] = function ($container) {
+            return function (
+                RequestInterface $Request,
+                ResponseInterface $Response,
+                $Exception
+            ) use ($container) {
+
+                if ($Exception instanceof QUI\Exception) {
+                    $result = array(
+                        'error' => $Exception->toArray()
+                    );
+
+                    QUI\System\Log::writeException(
+                        $Exception,
+                        QUI\System\Log::LEVEL_ERROR,
+                        array(
+                            'package' => 'quiqqer/rest'
+                        ),
+                        'rest.log'
+                    );
+
+                    return $Response->withStatus(500)
+                        ->withHeader('Content-Type', 'application/json')
+                        ->write(json_encode($result));
+                }
+
+                return $Response;
+            };
         };
 
         // config
@@ -68,25 +100,14 @@ class Server
      */
     public function run()
     {
-        $this->Slim->get('/', function (RequestInterface $Request, ResponseInterface $Response, $args) {
-            return $Response->write(
-                '<pre>
-         _______          _________ _______  _______  _______  _______
-        (  ___  )|\     /|\__   __/(  ___  )(  ___  )(  ____ \(  ____ )
-        | (   ) || )   ( |   ) (   | (   ) || (   ) || (    \/| (    )|
-        | |   | || |   | |   | |   | |   | || |   | || (__    | (____)|
-        | |   | || |   | |   | |   | |   | || |   | ||  __)   |     __)
-        | | /\| || |   | |   | |   | | /\| || | /\| || (      | (\ (
-        | (_\ \ || (___) |___) (___| (_\ \ || (_\ \ || (____/\| ) \ \__
-        (____\/_)(_______)\_______/(____\/_)(____\/_)(_______/|/   \__/
+        $self = $this;
 
-
-            Welcome to QUIQQER REST API.
-                
-                </pre>'
-            );
-        });
-
+        $this->Slim->get(
+            '/',
+            function (RequestInterface $Request, ResponseInterface $Response, $args) use ($self) {
+                return $self->help($Request, $Response, $args);
+            }
+        );
 
         // Hello World
         $this->Slim->get('/hello/{name}', function (RequestInterface $Request, ResponseInterface $Response, $args) {
@@ -94,7 +115,15 @@ class Server
         });
 
 
-        // register plugins
+        // packages provider
+        $provider = $this->getProvidersFromPackages();
+
+        /* @var $Provider ProviderInterface */
+        foreach ($provider as $Provider) {
+            $Provider->register($this);
+        }
+
+        // register middlewares
         $this->Slim->add(
             (new BasePath($this->config['basePath']))->autodetect(true)
         );
@@ -110,5 +139,98 @@ class Server
     public function getSlim()
     {
         return $this->Slim;
+    }
+
+    /**
+     * Return all provider from the packages
+     *
+     * @return array
+     */
+    protected function getProvidersFromPackages()
+    {
+        $packages = QUI::getPackageManager()->getInstalled();
+        $result   = array();
+
+        try {
+            $providerList = QUI\Cache\Manager::get('quiqqer/rest/providerList');
+        } catch (QUI\Cache\Exception $Exception) {
+            $providerList = array();
+
+            /* @var $Package QUI\Package\Package */
+            foreach ($packages as $package) {
+                $provider = QUI::getPackage($package['name'])->getProvider();
+                $provider = array_filter($provider, function ($key) {
+                    return $key === 'rest';
+                }, \ARRAY_FILTER_USE_KEY);
+
+                if (isset($provider['rest'])) {
+                    $providerList = array_merge($providerList, $provider['rest']);
+                }
+            }
+
+            QUI\Cache\Manager::set(
+                'quiqqer/rest/providerList',
+                $providerList
+            );
+        }
+
+        // initialize the instances
+        foreach ($providerList as $provider) {
+            try {
+                $Provider = new $provider();
+
+                if ($Provider instanceof ProviderInterface) {
+                    $result[] = $Provider;
+                }
+            } catch (\Exception $exception) {
+                QUI\System\Log::writeException($provider);
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Helper message for the request
+     *
+     * @param RequestInterface $Request
+     * @param ResponseInterface $Response
+     * @param $args
+     * @return mixed
+     */
+    protected function help(RequestInterface $Request, ResponseInterface $Response, $args)
+    {
+        $patterns = array();
+        $routes   = $this->Slim->getContainer()->get('router')->getRoutes();
+
+        foreach ($routes as $Route) {
+            if ($Route->getPattern() != '/') {
+                $patterns[] = $Route->getPattern();
+            }
+        }
+
+        $output = '<pre>
+  _______          _________ _______  _______  _______  _______
+ (  ___  )|\     /|\__   __/(  ___  )(  ___  )(  ____ \(  ____ )
+ | (   ) || )   ( |   ) (   | (   ) || (   ) || (    \/| (    )|
+ | |   | || |   | |   | |   | |   | || |   | || (__    | (____)|
+ | |   | || |   | |   | |   | |   | || |   | ||  __)   |     __)
+ | | /\| || |   | |   | |   | | /\| || | /\| || (      | (\ (
+ | (_\ \ || (___) |___) (___| (_\ \ || (_\ \ || (____/\| ) \ \__
+ (____\/_)(_______)\_______/(____\/_)(____\/_)(_______/|/   \__/
+
+
+ Welcome to QUIQQER REST API.
+
+
+';
+
+        foreach ($patterns as $pattern) {
+            $output .= ' - ' . $pattern . "\n\n";
+        }
+
+        $output .= '</pre>';
+
+        return $Response->write($output);
     }
 }
